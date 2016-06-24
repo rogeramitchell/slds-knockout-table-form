e6f5d4e2651f9fd0c7b328fef0c007edc3a0113c
Roger Mitchell <rogeramitchell@gmail.com>
2016-06-23 22:03:02 -0400

    creating JS model for table view - WIP

--- view.js
+++ view.js
@@ -1,1 +1,10 @@
-// maintains view model for our table
+var MonthTableViewModel = function(startDate, numberMonths, amount) {
+	this.startDate = ko.observable(startDate);
+	this.numberMonths = ko.observable(numberMonths);
+	this.amount = ko.observable(amount);
+	this.total = ko.computed(function() {
+		return this.numberMonths() * this.amount();
+	}, this);
+};
+
+ko.applyBindings(new MonthTableViewModel('1/1/2015', 3, 1500));
